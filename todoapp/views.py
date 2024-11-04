71f5e454221dfce1cf829a65b8c49e2ce2d03769
king0jin <et0709@naver.com>
2024-11-04 21:18:20 +0900

    모델(테이블)생성

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+#JSON리턴
+from django.http import JsonResponse
+#상태 코드 요청
+from rest_framework import status
+
+#클래스 요청 처리하기 위해서
+from django.views import View
+from .models import Todo
+
 
 # Create your views here.
+
+class TodoView(View):
+    #조회 요쳥 처리 함수
+    def get(self, request):
+        userid = request.GET.get("userid", None)
+        if userid != None:
+            todos = Todo.objects.filter(userid=userid)
+        else:
+            todos = Todo.objects.all()
+        return JsonResponse({'list':list(todos.values())}, status=status.HTTP_200_OK)
