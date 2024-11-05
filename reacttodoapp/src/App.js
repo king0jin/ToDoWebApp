cbbc53fc5c2ae9ed2420c6e557e780516946cd3d
king0jin <et0709@naver.com>
2024-11-05 21:37:19 +0900

    여러개 목록 보여주기

--- reacttodoapp/src/App.js
+++ reacttodoapp/src/App.js
@@ -5,13 +5,18 @@ import './App.css';
 class App extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {item: { id: 0, title: "Hello React", done: true}};
+    // 데이터 생성
+    this.state = {item: [{ id: 0, title: "황치즈쿠키먹기", done: false},
+                         { id: 1, title: "그릭요거트만들기", done: true}]};
   }
   
   render() {
+    //컴포넌트 만들기
+    let todoItems = this.state.item.map((item, idx) => (<ToDo item={item} key={item.id}/>));
+
     return (
       <div className="App">
-        <ToDo item={this.state.item} />
+        {todoItems}
       </div>
     );
   }
