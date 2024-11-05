63f4bba3bc626a440087f1ad81df7c6fb66599c2
king0jin <et0709@naver.com>
2024-11-05 21:23:32 +0900

    ToDo구성요소추가, App통합

--- reacttodoapp/src/App.js
+++ reacttodoapp/src/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import ToDo from "./ToDo";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <ToDo/>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {item: { id: 0, title: "Hello React", done: true}};
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <ToDo item={this.state.item} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- reacttodoapp/src/ToDo.jsx
+++ reacttodoapp/src/ToDo.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
 class ToDo extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { item: props.item };
+    }
+  
     render() {
-        return (
-            <div className="ToDo">
-                <input type="checkbox" id="todobox" name="todobox" value="todobox" />
-                <label for="todobox">ToDo 컴포넌트 만들기</label>
-            </div>
-         );
-     }
+      return (
+        <div className="ToDo">
+          <input 
+            type="checkbox" 
+            id={this.state.item.id} 
+            name={this.state.item.id} 
+            checked={this.state.item.done} 
+          />
+          <label htmlFor={this.state.item.id}>{this.state.item.title}</label>
+        </div>
+      );
+    }
 }
 export default ToDo;
