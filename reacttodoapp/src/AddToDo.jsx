de55aaa0be515e003eb6aafce806a54e88e92d2f
king0jin <et0709@naver.com>
2024-11-05 22:28:35 +0900

    데이터 추가 기능

--- reacttodoapp/src/AddToDo.jsx
+++ reacttodoapp/src/AddToDo.jsx
@@ -9,18 +9,46 @@ import {
 class AddTodo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { item: { title: " " } };
+        this.state = { item: { title: "" } };
+        this.add = props.add;
     }
+
+    //input의 내용이 변경되었을 때 호출되는 함수
+    onInputChange = (e) => {
+        this.setState({ item: { ...this.state.item, title: e.target.value } });
+    };
+    // + 버튼을 눌렀을 때 호출되는 함수
+    onButtonClick = () => {
+        console.log("Adding item:", this.state.item); // 확인용 로그
+        this.add(this.state.item);
+        this.setState({ item: { title: "" } });
+    };
+    //Enter를 눌렀을 때 호출되는 함수
+    enterKeyEventHandler = (e) => {
+        if (e.key === "Enter") {
+            this.onButtonClick(e);// + 버튼을 눌렀을 때 호출되는 함수
+        }
+    };
 
     render() {
         return (
             <Paper style={{ margin: 16, padding: 16 }}>
                 <Grid container>
                     <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-                        <TextField placeholder="Add Todo here" fullWidth/>
+                        <TextField 
+                            placeholder="Add Todo here" 
+                            fullWidth
+                            value={this.state.item.title}
+                            onChange={this.onInputChange}
+                            onKeyPress={this.enterKeyEventHandler}/>
                     </Grid>
-                    <Grid xs={1} md={1} item>
-                        <Button fullWidth color="secondary" variant="outlined"> + </Button>
+                    <Grid item xs={1} md={1}>
+                        <Button 
+                            fullWidth 
+                            color="secondary" 
+                            variant="outlined" 
+                            onClick={this.onButtonClick}
+                        > + </Button>
                     </Grid>
                 </Grid>
             </Paper>
--- reacttodoapp/src/App.js
+++ reacttodoapp/src/App.js
@@ -14,6 +14,21 @@ class App extends React.Component{
                          { id: 1, title: "그릭요거트만들기", done: true}]};
   }
   
+  //데이터 추가 함수
+  add = (item) => {
+    //리액트의 state와 props는 불변의 객체
+    //기존의 state에 있는 데이터들을 thisItems에 복사
+    const thisItems = this.state.item;
+    //새로운 데이터를 추가하기위한 초기상태 설정
+    item.id = "ID-" + thisItems.length;
+    //done 초기화
+    item.done = false;
+    //thisItems에 새로운 데이터 추가
+    thisItems.push(item); 
+    this.setState({ item: thisItems }); // 업데이트는 반드시 this.setState로 해야됨.
+    console.log("items : ", this.state.item);
+  };
+
   render() {
     //데이터가 있을 때 수행한다
     var todoItems = this.state.item.length > 0 && (
@@ -30,7 +45,7 @@ class App extends React.Component{
     return (
       <div className="App">
         <Container maxWidth="md">
-          <AddToDo/>
+          <AddToDo add={this.add}/>
           <div>{todoItems}</div>
         </Container>
       </div>
